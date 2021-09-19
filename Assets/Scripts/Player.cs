a3c888811fc862677e60874fab613cbdb372e66f
wekonu <wahjoe.eko.n@gmail.com>
2021-09-19 20:11:39 +0700

    [mod] implement player horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private const string WalkAnimation = "Walk";
+
+    private Rigidbody2D myBody;
+
+    private Animator animator;
+
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMoveKeyboard();
+    }
+
+    private void PlayerMoveKeyboard()
+    {
+        float movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
     }
 }
